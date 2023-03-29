5f25308be76477c1f9c983e10748c1801335d01d
Setare <zahrasadat.azarmsa@dci.education>
2023-03-29 22:13:00 +0200

    add server

--- server/index.js
+++ server/index.js
@@ -2,15 +2,16 @@ import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
 import postRouter from "./routes/postsRouter.js"
-
+import dotenv from "dotenv"
 const app = express()
-
+dotenv.config()
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
-app.use("/posts",postRouter)
-const CONNECTION_URL = "mongodb+srv://setazarm:herates66@dcicluster.y9yfftm.mongodb.net/?retryWrites=true&w=majority"
+app.use("/posts", postRouter)
+ const CONNECTION_URL =  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/?retryWrites=true&w=majority`
+
 const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() =>app.listen(PORT,()=>{console.log(`server running on port:${PORT}`)}))
-.catch((err)=>console.log(err.message));
+    .then(() => app.listen(PORT, () => { console.log(`server running on port:${PORT}`) }))
+    .catch((err) => console.log(err.message));
